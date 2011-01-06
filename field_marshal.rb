8f59b47002850add326063005c4d1643ba76e950
Jacob Rothstein <github@jacobrothstein.com>
2011-01-05 20:10:27 -0800

    [ fix up formatting ]
    it seems something in there confused emacs

--- field_marshal.rb
+++ field_marshal.rb
@@ -10,7 +10,8 @@ module FieldMarshal
     keys    = hash.keys.compact
     while keys.length > 0
       key   = keys.shift
-      value = hash[ key ]
+      value = hash[key]
+
       if keys.include? value
         keys.push key
       else
@@ -18,14 +19,14 @@ module FieldMarshal
       end
     end
 
-    [assigns.join("\n"), name ? name : hash[nil]].join("\n")
+    assigns.push(name ? name : hash[nil]).join("\n")
   end
 
   private
   def self.marshal_(thing, name = nil, already_marshalled = {})
     assigns_hash = {}
 
-    already_existing_var_name = already_marshalled.invert[ thing ]
+    already_existing_var_name = already_marshalled.invert[thing]
 
     if already_existing_var_name && name != already_existing_var_name
       assigns_hash[name] = already_existing_var_name
@@ -36,33 +37,36 @@ module FieldMarshal
 
       assigns_hash[name] =
         case thing
+  
         when Array
-          "[" + thing.map do |t|
-          assigns_hash.merge! marshal_(t, nil, already_marshalled)
-          assigns_hash.delete nil
-        end.join(", ") + "]"
-
-        when Hash
-          "{" + thing.map do |key, value|
-          [key, value].map do |t|
+          contents = thing.map do |t|
             assigns_hash.merge! marshal_(t, nil, already_marshalled)
             assigns_hash.delete nil
-          end.join(" => ")
-        end.join(", ") + "}"
+          end
+          "[ #{contents.join(",")} ]"
 
-        when nil, Symbol, String, Numeric, Hash then thing.inspect
+        when Hash
+          pairs = thing.map do |key, value|
+            [key, value].map do |t|
+              assigns_hash.merge! marshal_(t, nil, already_marshalled)
+              assigns_hash.delete nil
+            end.join(" => ")
+          end
+          "{ #{pairs.join ", "} }"
+
+        when nil, Symbol, String, Numeric then thing.inspect
 
         when Proc
           var_names = eval('local_variables', thing.binding).reject do |n|
-          already_marshalled.has_key? n
-        end
+            already_marshalled.has_key? n
+          end
 
           var_names.each do |var_name|
-          var = eval var_name, thing.binding
-          if var != thing
-            assigns_hash.merge! marshal_(var, var_name, already_marshalled)
+            var = eval var_name, thing.binding
+            if var != thing
+              assigns_hash.merge! marshal_(var, var_name, already_marshalled)
+            end
           end
-        end
 
           thing.to_ruby
         end
